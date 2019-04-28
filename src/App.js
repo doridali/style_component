adf6832e635feef1248c409a5d760fc9abf59d22
doridali <rahyu.js@gmail.com>
2019-04-29 02:29:11 +0900

    practicing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import styled, { createGlobalStyle, keyframes, css } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+createGlobalStyle`
+body{
+  padding:0;
+  margin:0;
+
+}`;
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>Hello </Button>
+        <Button danger rotationTime={5}>
+          Hello{" "}
+        </Button>
+        <Anchor href="http://google.com">Go to google</Anchor>
+      </Container>
+    );
+  }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#bf34fc" : "#2bc534")};
+
+  ${props => {
+    if (props.danger) {
+      return css`
+        animation: ${rotation} ${props.rotationTime}s linear infinite;
+      `;
+    }
+  }}
+`;
+const Anchor = styled(Button.withComponent("a"))`
+  text-decoration: none;
+`;
+
+const rotation = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to{
+  transform: rotate(360deg);
+}`;
 
 export default App;
